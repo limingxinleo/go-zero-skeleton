61a70f583dcadfb9568ccbf94f1db2aa0bb08fdc
李铭昕 <715557344@qq.com>
2024-12-01 10:33:36 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,1 @@
+# go-zero skeleton
---
+++ internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"main/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
