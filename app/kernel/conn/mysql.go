2e5195589f5e47673ff1115f58d0ac1fc32eecbb
李铭昕 <715557344@qq.com>
2024-12-17 14:19:08 +0800

    增加 Gorm 用法

--- app/kernel/conn/mysql.go
+++ app/kernel/conn/mysql.go
@@ -2,11 +2,20 @@ package conn
 
 import (
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	//"gorm.io/driver/mysql"
+	//"gorm.io/gorm"
 	"main/app/config"
 )
 
 var MySQL sqlx.SqlConn
 
+//var Gorm *gorm.DB
+
 func InitMySQL() {
 	MySQL = sqlx.NewMysql(config.Conf.MySqlConf.Dsn)
+	//Gorm, _ = gorm.Open(mysql.Open(config.Conf.MySqlConf.Dsn), &gorm.Config{})
 }
+
+//func NewGormDryRunSession() *gorm.DB {
+//	return Gorm.Session(&gorm.Session{DryRun: true})
+//}
