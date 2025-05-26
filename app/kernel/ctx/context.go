ec35ad2d2f6fdeb71ba24f96d4d199c2d537e699
李铭昕 <715557344@qq.com>
2025-05-26 17:35:53 +0800

    Added `ContextContainer`.

---
+++ app/kernel/ctx/context.go
@@ -0,0 +1,28 @@
+package ctx
+
+import (
+	"context"
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+const KeyLogger = "ctx.logger"
+
+type ContextContainer struct {
+	ctx context.Context
+}
+
+func (c *ContextContainer) Logger() logx.Logger {
+	return Logger(c.ctx)
+}
+
+func NewContext(cc context.Context) context.Context {
+	return context.WithValue(cc, KeyLogger, logx.WithContext(cc))
+}
+
+func Logger(cc context.Context) logx.Logger {
+	if logger, ok := cc.Value(KeyLogger).(logx.Logger); ok {
+		return logger
+	}
+
+	return logx.WithContext(cc)
+}
--- app/kernel/middleware.go
+++ app/kernel/middleware.go
@@ -1,11 +1,13 @@
 package kernel
 
 import (
+	"main/app/kernel/ctx"
 	"net/http"
 )
 
 func ServerMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		r = r.WithContext(ctx.NewContext(r.Context()))
 		next(w, r)
 		w.Header().Add("Server", "go-zero")
 	}
