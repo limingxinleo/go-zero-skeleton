e44cd231019776a17eaa85e0efc68b8f3ca3c3c0
李铭昕 <715557344@qq.com>
2026-01-04 20:19:45 +0800

    删除无用代码

--- app/bootstrap.go
+++ app/bootstrap.go
@@ -1,13 +1,6 @@
 package app
 
-import (
-	"github.com/limingxinleo/go-zero-skeleton/app/svc"
-)
-
-var ServiceContext *svc.ServiceContext
-
 func init() {
-	// 从环境变量中读取配置文件路径
 	app = NewApplication()
 
 	app.Init()
